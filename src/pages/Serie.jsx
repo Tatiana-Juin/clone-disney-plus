774610d98ab337523da3380a8e1d8fc1a7574ee7
Tatiana-Juin <username@users.noreply.github.com>
2025-09-03 01:21:21 +0200

    J'ai reussi toutes les series

--- src/pages/Serie.jsx
+++ src/pages/Serie.jsx
@@ -1,9 +1,19 @@
-
+import disneyPlusData from "../data/disneyPlusData"
 export default function Serie() {
   return (
     <div>
-      <p style={{color:"white"}}> Serie</p>
+      {/* <p style={{color:"white"}}> Serie</p> */}
       
+      {/* POR AFFICHER UNIQUEMENT LES SERIES  */}
+      {Object.entries(disneyPlusData).map(([categorie,contenus]) =>(
+        <div key={categorie}>
+            <ul>
+              {contenus.filter((disneySerie) => disneySerie.type==="serie" ).map((disneySerie) =>(
+                <li style={{color:"white"}} key={disneySerie.id}> {disneySerie.nom} </li>
+              ))}
+            </ul>
+        </div>
+      ))}
     </div>
   )
 }
