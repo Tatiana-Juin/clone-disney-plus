7c8f39627573f4592bda8576dce3a921b1df7dcf
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-09-03 12:22:00 +0200

    J'ai appeller mon composant CategoryList sur toutes les page que j'avais besoin pour afficher les film et serie marvel, disney , star etc

--- src/pages/NationalGeographic.jsx
+++ src/pages/NationalGeographic.jsx
@@ -1,7 +1,10 @@
-
+import disneyPlusData from "../data/disneyPlusData"
+import CategoryList from "../components/CategoryList"
 
 export default function NationalGeographic() {
   return (
-    <div>NationalGeographic</div>
+    <div>
+      <CategoryList data={disneyPlusData} cat="nationalGeographic" />
+    </div>
   )
 }
