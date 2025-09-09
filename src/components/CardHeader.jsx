1dbdb4939beecc43163cc0156db56459cc5c24f7
Tatiana-Juin <username@users.noreply.github.com>
2025-09-09 13:50:14 +0200

    Je commence le style de la cardHeader . Pour cela j'ai créer un fichier cardHeader.css

--- src/components/CardHeader.jsx
+++ src/components/CardHeader.jsx
@@ -1,8 +1,8 @@
-
+import './cardHeader.css'
 export default function CardHeader({title}) {
   return (
-    <div style={{backgroundColor:"#2D2F36",width:"10vw",marginRight:"10px"}}>
-        <p style={{color:'white',padding:"20px",textAlign:"center"}}> {title}</p>
+    <div className='card'>
+        <p className='card-information'> {title}</p>
     </div>
   )
 }
