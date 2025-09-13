fb901924cf7087b02a8da964b853911b4e6deedd
Tatiana-Juin <username@users.noreply.github.com>
2025-09-13 17:43:09 +0200

    J'ai modifier le nom des classes dans le fichier CardHeader,CategoryList et card.css

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -2,11 +2,11 @@ import './card.css'
 
 export default function CategoryList({data,cat}) {
   return (
-    <div className='div-category'>
+    <div className='category'>
          {Object.entries(data).filter(([categorie]) => categorie===cat).map(([categorie,contenus]) =>(
                     contenus.map((item) =>(
                       
-                        <div className='card-categorie' key={item.id}>
+                        <div className='card-category' key={item.id}>
                             <p className='text-information'> {item.nom} </p>
                         </div>
                     ))
